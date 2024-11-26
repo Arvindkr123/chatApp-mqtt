bba18293276fa74b52205a47b29e4ce13ee56356
arvindkr123 <thakurarvindkr10@gmail.com>
2024-11-26 11:30:44 +0530

    added change

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -1,11 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  lastSeen: { type: Date, default: Date.now }
+  lastSeen: { type: Date, default: Date.now },
 });
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
